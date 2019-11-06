29762dbd7126e7c8dd8f9d25319c4a847f7b5e19
mkolpin <michael.kolpin@gmail.com>
2019-11-06 21:51:20 +0100

    After first migration

--- hello/urls.py
+++ hello/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from hello import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("hello/<name>", views.hello_there, name="hello_there"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
 ]
+urlpatterns += staticfiles_urlpatterns()
--- hello/views.py
+++ hello/views.py
@@ -1,22 +1,23 @@
 import re
 from datetime import datetime
 from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Hello, Django!")
+from django.shortcuts import render
 
 def hello_there(request, name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
 
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
-    match_object = re.match("[a-zA-Z]+", name)
+def home(request):
+    return render(request, "hello/home.html")
 
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
+def about(request):
+    return render(request, "hello/about.html")
 
-    content = "Hello there, " + clean_name + "! It's " + formatted_now
-    return HttpResponse(content)
+def contact(request):
+    return render(request, "hello/contact.html")
